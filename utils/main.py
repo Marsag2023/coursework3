74d6c8dabc43e43a91ffcc76ecceea855e6e9c7f
Mars Aygistov <m.aygistov@gmail.com>
2024-01-15 20:17:14 +0300

    update

--- utils/main.py
+++ utils/main.py
@@ -1,5 +1,6 @@
-from functions import sort_operations, print_result
+from functions import json_operations, sort_operations, print_result
 
-data_operations = sort_operations()
+file_convert = json_operations()
+data_operations = sort_operations(file_convert)
 for dat in data_operations:
     print_result(dat)
